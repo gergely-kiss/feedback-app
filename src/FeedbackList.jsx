c595324e51863390b498e8d7656861535261a92e
Gergely Kiss <kiss-gergely@outlook.com>
2022-05-12 22:02:05 +0100

    delete x

--- src/FeedbackList.jsx
+++ src/FeedbackList.jsx
@@ -1,12 +1,18 @@
 import FeedbackItem from './FeedbackItem'
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
 	if (!feedback || feedback.length === 0) {
 		return <p> no feedback yet</p>
 	}
 	return (
-		<div className='feedback-list'>{feedback.map((item) => 
-            <FeedbackItem key={item.id} item = {item} />
-            )}</div>
+		<div className='feedback-list'>
+			{feedback.map((item) => (
+				<FeedbackItem
+					key={item.id}
+					item={item}
+					handleDelete={handleDelete}
+				/>
+			))}
+		</div>
 	)
 }
 
